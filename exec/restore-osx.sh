31e7ef494df8a04f09389209c218f084cf595bd5
Di Zhou <diz@a19901diz.office-people-doc.com>
2022-02-20 17:58:36 -0500

    remove unsed file

--- exec/restore-osx.sh
+++
@@ -1,5 +0,0 @@
-cp .bash_profile ~/.bash_profile &&
-cp starship.toml ~/.config/starship.toml &&
-cp alacritty.yml ~/.config/alacritty/alacritty.yml &&
-cp keymap.json ~/.config/joplin/keymap.json &&
-cp ~/projects/configs/.tmux.conf ~/.tmux.conf 
