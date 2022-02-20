e40f8d75beee85f09102137dd2166bee0e8579b0
Di Zhou <diz@a19901diz.office-people-doc.com>
2022-02-20 17:56:17 -0500

    add nvim config

--- exec/backup.sh
+++ exec/backup.sh
@@ -1,23 +1,30 @@
 #!/usr/bin/env bash
 
+echo "Backing up"
+
+# Platform specific 
 if [ "$(uname)" == "Darwin" ]; then
     # Do something under Mac OS X platform        
-cp ~/.bash_profile ~/projects/configs/.bash_profile &&
-cp ~/.config/starship.toml ~/projects/configs/starship.toml &&
-cp ~/.config/alacritty/alacritty.yml ~/projects/configs/alacritty.yml &&
-cp ~/.config/joplin/keymap.json ~/projects/configs/keymap.json &&
-cp ~/.tmux.conf ~/projects/configs/.tmux.conf 
+    cp ~/.bash_profile ~/projects/configs/.bash_profile
 
 elif  [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     # Do something under GNU/Linux platform
-cp ~/.bashrc ~/projects/configs/.bashrc &&
-cp ~/.config/starship.toml ~/projects/configs/starship.toml &&
-cp ~/.config/alacritty/alacritty.yml ~/projects/configs/alacritty.yml &&
-cp ~/.config/joplin/keymap.json ~/projects/configs/keymap.json &&
-cp ~/.tmux.conf ~/projects/configs/.tmux.conf 
-# elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    cp ~/.bashrc ~/projects/configs/.bashrc
+      
+# elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
     # Do something under 32 bits Windows NT platform
-    # else [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; 
+    # else [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; 
     # Do something under 64 bits Windows NT platform
 fi
 
+cp ~/.tmux.conf  ~/projects/configs/.tmux.conf &&
+
+cp ~/.config/starship.toml  ~/projects/configs/starship.toml &&
+
+cp ~/.config/alacritty/alacritty.yml  ~/projects/configs/alacritty.yml &&
+
+cp ~/.config/joplin/keymap.json  ~/projects/configs/keymap.json &&
+
+cp -rf ~/.config/nvim  ~/projects/configs/nvim
+
+echo "done"
--- exec/restore.sh
+++ exec/restore.sh
@@ -1,5 +1,29 @@
-cp .bashrc ~/.bashrc &&
-cp starship.toml ~/.config/starship.toml &&
-cp alacritty.yml ~/.config/alacritty/alacritty.yml &&
-cp keymap.json ~/.config/joplin/keymap.json &&
-cp ~/projects/configs/.tmux.conf ~/.tmux.conf 
+#!/usr/bin/env bash
+
+echo "Restoring"
+
+dir_source = "~/projects/configs/"
+dir_destination = "~/.config/"
+
+# Platform specific 
+if [ "$(uname)" == "Darwin" ]; then
+    # Do something under Mac OS X platform        
+    cp ${dir_source}.bash_profile       ~/.bash_profile
+
+elif  [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # Do something under GNU/Linux platform
+    cp ${dir_source}.bashrc     ~/.bashrc
+      
+# elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    # Do something under 32 bits Windows NT platform
+    # else [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; 
+    # Do something under 64 bits Windows NT platform
+fi
+
+cp      ${dir_source}.tmux.conf             ~/.tmux.conf &&
+cp      ${dir_source}starship.toml          ${dir_destination}starship.toml  &&
+cp      ${dir_source}alacritty.yml          ${dir_destination}alacritty/alacritty.yml  &&
+cp      ${dir_source}keymap.json            ${dir_destinatin}joplin/keymap.json &&
+cp -rf  ${dir_source}nvim                   ${dir_destination}nvim
+
+echo "done"
