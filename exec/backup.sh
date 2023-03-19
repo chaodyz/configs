ed3710c90bf95c6f0faef8fd811ac3e2f402a49e
Di Zhou <di.zhou.517@gmail.com>
2023-03-19 14:44:52 -0400

    chore(emacs): clean up

--- exec/backup.sh
+++ exec/backup.sh
@@ -1,36 +1,51 @@
 #!/usr/bin/env bash
 
-echo "Backing up"
+function backup_action() {
+    echo "Backing up"
+    # Platform specific 
+    if [ "$(uname)" == "Darwin" ]; then
+	# when its macos we copy both bashrc and profile
+	cp ~/.bash_profile ~/projects/configs/.bash_profile &&
+	    cp ~/.bashrc ~/projects/configs/.bashrc
+	
+    elif  [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+	# Do something under GNU/Linux platform
+	cp ~/.bashrc ~/projects/configs/.bashrc
+	
+	# elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+	# Do something under 32 bits Windows NT platform
+	# else [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; 
+	# Do something under 64 bits Windows NT platform
+    fi
 
-# Platform specific 
-if [ "$(uname)" == "Darwin" ]; then
-    # when its macos we copy both bashrc and profile
-    cp ~/.bash_profile ~/projects/configs/.bash_profile &&
-    cp ~/.bashrc ~/projects/configs/.bashrc
+    cp ~/.tmux.conf  ~/projects/configs/.tmux.conf &&
+	
+	cp ~/.config/starship.toml  ~/projects/configs/starship.toml &&
+	
+	cp ~/.config/alacritty/alacritty.yml  ~/projects/configs/alacritty.yml &&
 
-elif  [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    # Do something under GNU/Linux platform
-    cp ~/.bashrc ~/projects/configs/.bashrc
-      
-# elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
-    # Do something under 32 bits Windows NT platform
-    # else [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; 
-    # Do something under 64 bits Windows NT platform
-fi
+	cp ~/.config/joplin/keymap.json  ~/projects/configs/keymap.json &&
 
-cp ~/.tmux.conf  ~/projects/configs/.tmux.conf &&
+	cp -rf ~/.config/nvim  ~/projects/configs/ &&
+	
+	cp ~/.emacs.d/init.el ~/projects/configs/emacs/init.el &&
 
-cp ~/.config/starship.toml  ~/projects/configs/starship.toml &&
+	# backup org files
+	cp ~/eSync/org/emacs.org ~/projects/configs/emacs/emacs.org
 
-cp ~/.config/alacritty/alacritty.yml  ~/projects/configs/alacritty.yml &&
+    echo "done"
+}
 
-cp ~/.config/joplin/keymap.json  ~/projects/configs/keymap.json &&
+read -p "Do you want to proceed backup, this will reset your projects/config? (y/n) " choice
 
-cp -rf ~/.config/nvim  ~/projects/configs/ &&
+case "$choice" in
+    y|Y ) echo "Proceeding to the backup action."
+	  backup_action
+	  ;;
+    n|N ) echo "Exiting the backup script." && exit;;
+    * ) echo "Invalid choice. Exiting the backup script." && exit;;
+esac
 
-cp ~/.emacs.d/init.el ~/projects/configs/emacs/init.el &&
 
-# backup org files
-cp ~/eSync/org/emacs.org ~/projects/configs/emacs/emacs.org
 
-echo "done"
+
--- exec/restore.sh
+++ exec/restore.sh
@@ -1,30 +1,41 @@
 #!/usr/bin/env bash
 
-echo "Restoring"
+function restore_action() {
+    echo "Restoring"
+    # Platform specific
+    if [ "$(uname)" == "Darwin" ]; then
+	# Do something under Mac OS X platform       
+	cp ~/projects/configs/.bash_profile       ~/.bash_profile &&
+	    cp ~/projects/configs/.bashrc      ~/.bashrc
 
-# Platform specific
-if [ "$(uname)" == "Darwin" ]; then
-   # Do something under Mac OS X platform       
-   cp ~/projects/configs/.bash_profile       ~/.bash_profile &&
-   cp ~/projects/configs/.bashrc      ~/.bashrc
+    elif  [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+	# Do something under GNU/Linux platform
+	cp ~/projects/configs/.bashrc     ~/.bashrc
+	
+	# elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+	# Do something under 32 bits Windows NT platform
+	# else [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ];
+	# Do something under 64 bits Windows NT platform
+    fi
 
-elif  [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-   # Do something under GNU/Linux platform
-   cp ~/projects/configs/.bashrc     ~/.bashrc
-    
-# elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
-   # Do something under 32 bits Windows NT platform
-   # else [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ];
-   # Do something under 64 bits Windows NT platform
-fi
+    cp      ~/projects/configs/.tmux.conf             ~/.tmux.conf  &&
+	cp      ~/projects/configs/starship.toml          ~/.config/starship.toml  &&
+	cp      ~/projects/configs/alacritty.yml          ~/.config/alacritty/alacritty.yml  &&
+	cp      ~/projects/configs/keymap.json            ~/.config/joplin/keymap.json &&
+	rm -rf ~/.config/nvim/ &&
+	cp -rf  ~/projects/configs/nvim                   ~/.config/ &&
+	cp      ~/projects/configs/emacs/init.el          ~/.emacs.d/init.el &&
+	cp      ~/projects/configs/emacs/emacs.org        ~/eSync/org/emacs.org
 
-cp      ~/projects/configs/.tmux.conf             ~/.tmux.conf  &&
-cp      ~/projects/configs/starship.toml          ~/.config/starship.toml  &&
-cp      ~/projects/configs/alacritty.yml          ~/.config/alacritty/alacritty.yml  &&
-cp      ~/projects/configs/keymap.json            ~/.config/joplin/keymap.json &&
-rm -rf ~/.config/nvim/ &&
-cp -rf  ~/projects/configs/nvim                   ~/.config/ &&
-cp      ~/projects/configs/emacs/init.el          ~/.emacs.d/init.el &&
-cp      ~/projects/configs/emacs/emacs.org        ~/eSync/org/emacs.org
+    echo "done"
+}
 
-echo "done"
+read -p "Do you want to proceed restore, this will reset all your local files with the files from configs? (y/n) " choice
+
+case "$choice" in
+    y|Y ) echo "Proceeding to the restore action."
+	  restore_action
+	  ;;
+    n|N ) echo "Exiting the restore script." && exit;;
+    * ) echo "Invalid choice. Exiting the restore script." && exit;;
+esac
