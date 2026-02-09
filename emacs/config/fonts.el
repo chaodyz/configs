6ab4cec427a953e278dcf9079ba8f2f53832149c
di zhou <di.zhou.517@gmail.com>
2026-02-09 01:32:14 -0500

    adjust text sacle

--- emacs/config/fonts.el
+++ emacs/config/fonts.el
@@ -9,13 +9,12 @@
 
   ;; Font
   ;; Set font by different mode: (set-face-attribute 'default nil :font "FONT_NAME":height: FONT_SIZE MODE/BUFFER)
-  (set-face-attribute 'default nil :font "FiraCode Nerd Font Mono" :height 180)
+  (set-face-attribute 'default nil :font "FiraCode Nerd Font Mono" :height 120)
   (set-face-attribute 'fixed-pitch nil :font "FiraCode Nerd Font Mono" :height 160)
-  (set-face-attribute 'variable-pitch nil :family "Source Sans 3" :height 180 :weight 'normal)
+  (set-face-attribute 'variable-pitch nil :family "Source Sans 3" :height 160 :weight 'normal)
 
 ;; 汉字－思源黑体
   (set-fontset-font t 'han (font-spec :family "Source Han Sans SC VF"))
-  (set-face-attribute 'variable-pitch nil :family "Source Sans 3" :height 180 :weight 'normal)
 
 (provide 'fonts)
 ;;; fonts.el ends here
