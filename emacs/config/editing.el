47f29d35264c6ba48b2d4ff346dcdaf7f439617b
Di Zhou <dzhou@perceptyx.com>
2026-03-08 23:27:09 -0400

    format

--- emacs/config/editing.el
+++ emacs/config/editing.el
@@ -41,69 +41,69 @@
 ;; =============================================================================
 
 
-  (use-package evil
-    :init
-    (setq evil-undo-system 'undo-tree);; tell Evil to use undo-tree
-    (setq evil-want-integration t)
-    (setq evil-want-keybinding nil) (setq evil-want-C-u-scroll t)
-    (setq evil-want-C-i-jump t)
-    :config
-    (evil-mode 1)
-    (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
-    (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+(use-package evil
+  :init
+  (setq evil-undo-system 'undo-tree);; tell Evil to use undo-tree
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil) (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump t)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
 
-    ;; Use visual line motions even outside of visual-line-mode buffers
-    (evil-global-set-key 'motion "j" 'evil-next-visual-line)
-    (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
 
-    (evil-set-initial-state 'messages-buffer-mode 'normal)
-    (evil-set-initial-state 'dashboard-mode 'normal)
-    )
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal)
+  )
 
 
-  (use-package evil-nerd-commenter
-    ;; :config
-    ;; (evilnc-default-hotkeys)
-    )
-  (define-key evil-normal-state-map "gc" 'evilnc-comment-or-uncomment-lines)
+(use-package evil-nerd-commenter
+  ;; :config
+  ;; (evilnc-default-hotkeys)
+  )
+(define-key evil-normal-state-map "gc" 'evilnc-comment-or-uncomment-lines)
 
-  (use-package evil-collection
-    :after evil
-    :config
-    (delete 'magit evil-collection-mode-list) ;; defer magit to avoid load-order issues
-    (evil-collection-init))
+(use-package evil-collection
+  :after evil
+  :config
+  (delete 'magit evil-collection-mode-list) ;; defer magit to avoid load-order issues
+  (evil-collection-init))
 
-  ;; Init magit evil bindings only after magit actually loads
-  (with-eval-after-load 'magit
-    (evil-collection-init '(magit magit-todos)))
+;; Init magit evil bindings only after magit actually loads
+(with-eval-after-load 'magit
+  (evil-collection-init '(magit magit-todos)))
 
-  ;; Escape key to quit menu
-  (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+;; Escape key to quit menu
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
-  ;; ;; Distinguish C-i from TAB in GUI Emacs so evil-jump-forward works
-  ;; (when (display-graphic-p)
-  ;;   (define-key input-decode-map "\C-i" [C-i])
-  ;;   (with-eval-after-load 'evil
-  ;;     (define-key evil-motion-state-map [C-i] 'evil-jump-forward)))
+;; ;; Distinguish C-i from TAB in GUI Emacs so evil-jump-forward works
+;; (when (display-graphic-p)
+;;   (define-key input-decode-map "\C-i" [C-i])
+;;   (with-eval-after-load 'evil
+;;     (define-key evil-motion-state-map [C-i] 'evil-jump-forward)))
 
-  ;; Xref keybindings for Evil mode
-  (with-eval-after-load 'evil
-    (define-key evil-normal-state-map (kbd "gd") #'xref-find-definitions)
-    (define-key evil-normal-state-map (kbd "gr") #'xref-find-references))
+;; Xref keybindings for Evil mode
+(with-eval-after-load 'evil
+  (define-key evil-normal-state-map (kbd "gd") #'xref-find-definitions)
+  (define-key evil-normal-state-map (kbd "gr") #'xref-find-references))
 
 ;; =============================================================================
 ;; Hydra (Text Scaling)
 ;; =============================================================================
 
 
-  (use-package hydra)
-  
-  (defhydra hydra-text-scale (:timeout 4)
-    "scale text"
-    ("j" text-scale-increase "in")
-    ("k" text-scale-decrease "out")
-    ("q" nil "finished" :exit t))
-  (global-set-key (kbd "C-c t") 'hydra-text-scale/body)
+(use-package hydra)
+
+(defhydra hydra-text-scale (:timeout 4)
+  "scale text"
+  ("j" text-scale-increase "in")
+  ("k" text-scale-decrease "out")
+  ("q" nil "finished" :exit t))
+(global-set-key (kbd "C-c t") 'hydra-text-scale/body)
 
 ;; =============================================================================
 ;; Indentation & Formatting
@@ -118,14 +118,14 @@
 ;; Other Experiments / Settings
 ;; =============================================================================
 
-  ;; French typing experience
-  (setq mac-right-option-modifier nil) ;; Right Option behaves as Option
+;; French typing experience
+(setq mac-right-option-modifier nil) ;; Right Option behaves as Option
 
-    ;; TODO: Try to mimic move lines action
-    ;; keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
-    ;; keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
-    ;; keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
-    ;; keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+;; TODO: Try to mimic move lines action
+;; keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+;; keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+;; keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+;; keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
 (provide 'editing)
 ;;; editing.el ends here
